de089ef68c439332bf4a7cd2712578c6e635673e
chochojj <csgy114@gmail.com>
2023-08-31 23:00:01 +0900

    feat: state로 데이터 렌더링

--- src/pages/IssueDetailPage.tsx
+++ src/pages/IssueDetailPage.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { styled } from "styled-components";
+
+import { Issue } from "../models/IIssue";
+
+interface RouteState {
+  state?: {
+    issue: Issue;
+  };
+}
+
 const IssueDetailPage = () => {
-  return <></>;
+  const { state } = useLocation() as RouteState;
+  const [issue, setIssue] = useState<Issue | undefined>(state?.issue);
+  const param = useParams<{ issueId: string }>();
+
+  console.log(state);
+  console.log(param);
+
+  return (
+    <Wrapper>
+      <Content />
+    </Wrapper>
+  );
 };
 
 export default IssueDetailPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  width: 960px;
+`;
