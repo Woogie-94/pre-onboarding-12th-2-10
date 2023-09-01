41d0574b6aef6ef57e8734f1479575a14b31167d
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-01 17:03:03 +0900

    feat: 이슈 페이지에 인피니티 스크롤이 가능하도록 useObserver를 사용

--- src/pages/IssuePage.tsx
+++ src/pages/IssuePage.tsx
@@ -5,15 +5,28 @@ import Skeleton from "../components/common/Skeleton";
 import AdItem from "../components/issue/AdItem";
 import IssueItem from "../components/issue/IssueItem";
 import { CONTENT_COUNT } from "../constants/issue";
+import useObserver from "../hooks/useObserver";
 import usePaginationFetch from "../hooks/usePaginationFetch";
 import { getIssueList } from "../services/issue";
 
 const IssuePage = () => {
-  const { data: issueList, fetch, isLoading } = usePaginationFetch({ initialData: [], fetchFn: getIssueList });
+  const { entry, setTarget } = useObserver({ rootMargin: "300px" });
+  const {
+    data: issueList,
+    fetch,
+    fetchNextPage,
+    isLoading,
+  } = usePaginationFetch({ initialData: [], fetchFn: getIssueList });
 
   useEffect(() => {
     fetch();
   }, [fetch]);
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
 
   return (
     <Wrapper>
@@ -21,7 +34,7 @@ const IssuePage = () => {
         <ul>
           {issueList.map((issue, index) => (
             <Fragment key={issue.id}>
-              <IssueItem issue={issue} />
+              <IssueItem issue={issue} ref={issueList.length - 1 === index ? setTarget : null} />
               {!((index + 1) % CONTENT_COUNT) && <AdItem />}
             </Fragment>
           ))}
